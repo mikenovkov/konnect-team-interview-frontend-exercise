85497cf9920a5d5ec5e45430f1b37fe6a966bf9f
mikenovkov <michaelnov86@gmail.com>
2022-11-06 21:56:38 -0500

    feat: add service catalog list,details page, search,pagination

---
+++ src/components/common/Spinner.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="loader" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Loader',
+})
+</script>
+
+<style lang="scss" scoped>
+    .loader {
+        position: relative;
+        width: 30px;
+        height: 30px;
+        margin: 10px auto;
+        &::after {
+            display: block;
+            width: 100%;
+            height: 100%;
+            margin: 1px;
+            border: 3px solid;
+            border-color: #072863 transparent #072863 transparent;
+            border-radius: 50%;
+            animation: rings 1.5s ease-in-out infinite;
+            content: ' ';
+        }
+    }
+
+    @keyframes rings {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+</style>
---
+++ src/components/serviceCatalogList/ServiceCatalogList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="catalog-list">
+        <div class="service-list__count">
+            {{ services.length }} Services
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ServiceCatalogList',
+  props: {
+      services: {
+          type: Array,
+          required: true,
+          default: () => [],
+      },
+  },
+  setup() {
+    return {
+
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.catalog-list {
+   display: grid;
+   margin: 20px 0 0 0;
+   grid-template-columns: 1fr 1fr 1fr;
+   gap: 40px;
+}
+</style>
